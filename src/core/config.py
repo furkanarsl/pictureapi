63cb27d930e33b00b46d6b4ed00723892d9366f7
Furkan <frkn__asl@windowslive.com>
2021-03-31 05:42:56 +0300

    config variables for application

---
+++ src/core/config.py
@@ -0,0 +1,15 @@
+import secrets
+from datetime import timedelta
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    authjwt_secret_key: str = "56691a33a3124999f6a896e2812d9d9f7f5eabc33c1cc56b" # TODO move to .env
+    authjwt_access_token_expires: timedelta = timedelta(hours=3)
+    authjwt_refresh_token_expires: timedelta = timedelta(days=30)
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = 'postgresql://furkana:0071@localhost:5432/postgres' # TODO move to .env
+
+settings = Settings()
