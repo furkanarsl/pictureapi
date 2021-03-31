379a1e2d85138b14a640fe4bf17588ddffbab5dd
Furkan <frkn__asl@windowslive.com>
2021-03-31 05:44:43 +0300

    jwt authentication setup

--- src/main.py
+++ src/main.py
@@ -1,4 +1,24 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Request
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+
 from src.api.v1.api import api_router
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from src.core.config import settings
+
 app = FastAPI()
-app.include_router(api_router, prefix='/api/v1')
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
+
+@AuthJWT.load_config
+def get_config():
+    return settings
+
+
+@app.exception_handler(AuthJWTException)
+def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
+    )
