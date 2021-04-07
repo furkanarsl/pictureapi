a7196b62d33de4f87f11a75fd243a9536cb8ccb8
Furkan <frkn__asl@windowslive.com>
2021-04-07 10:44:04 +0300

    fix token time

--- src/core/config.py
+++ src/core/config.py
@@ -8,7 +8,7 @@ from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, v
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     authjwt_secret_key: str = "56691a33a3124999f6a896e2812d9d9f7f5eabc33c1cc56b" # TODO move to .env
-    authjwt_access_token_expires: timedelta = timedelta(minutes=2)
+    authjwt_access_token_expires: timedelta = timedelta(hours=5)
     authjwt_refresh_token_expires: timedelta = timedelta(days=30)
     SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = 'postgres://furkana:0071@localhost:5432/postgres' # TODO move to .env
 
