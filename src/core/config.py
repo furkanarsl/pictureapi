a79e9ac74199b480a3cfd137efa680ee085417e9
Furkan <frkn__asl@windowslive.com>
2021-04-09 18:23:47 +0300

    move config to env variables

--- src/core/config.py
+++ src/core/config.py
@@ -2,15 +2,17 @@ import secrets
 from datetime import timedelta
 from typing import Any, Dict, List, Optional, Union
 
-from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
+from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator, DirectoryPath
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    authjwt_secret_key: str = "56691a33a3124999f6a896e2812d9d9f7f5eabc33c1cc56b" # TODO move to .env
-    authjwt_access_token_expires: timedelta = timedelta(hours=5)
-    authjwt_refresh_token_expires: timedelta = timedelta(days=30)
-    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = 'postgres://furkana:0071@localhost:5432/postgres' # TODO move to .env
+    authjwt_secret_key: str
+    authjwt_access_token_expires: Union[timedelta, int]
+    authjwt_refresh_token_expires: Union[timedelta, int]
+    SQLALCHEMY_DATABASE_URI: PostgresDsn  # TODO move to .env
 
+
+SAVE_PATH: DirectoryPath = 'saved'
 
 settings = Settings()
