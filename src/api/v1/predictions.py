ada02492bcf8c564e75adb1b43e3658937f12e56
Furkan <frkn__asl@windowslive.com>
2021-04-09 18:23:15 +0300

    fix file save path and add protection to endpoint

--- src/api/v1/predictions.py
+++ src/api/v1/predictions.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter, UploadFile, File, Form, Body, HTTPException, status
+from fastapi import APIRouter, UploadFile, File, Form, Body, HTTPException, status, Depends
 import aiofiles
+from fastapi_jwt_auth import AuthJWT
+
 from src.schemas.prediction import PredictionParameters, PredictionResult
+import pathlib
+from src.services.result_log import log_service
+from src.services.user import user_service
+from src.core.config import settings
 
 ALLOWED_EXTENSIONS = ["jpg", "jpeg", "png"]
 router = APIRouter()
@@ -14,7 +20,7 @@ async def file_upload(file: UploadFile = File(...)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not a valid file.")
     if file_extension not in ALLOWED_EXTENSIONS:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="File type not allowed.")
-    save_path = file_name + "." + file_extension
+    save_path = pathlib.Path(settings.SAVE_PATH) / (file_name + "." + file_extension)
     async with aiofiles.open(save_path, 'wb') as out_file:
         while content := await file.read(16777216):  # 16MB
             await out_file.write(content)
@@ -22,6 +28,9 @@ async def file_upload(file: UploadFile = File(...)):
 
 
 @router.post("/", response_model=PredictionResult)
-async def predict(body: PredictionParameters):
+async def predict(body: PredictionParameters, authorize: AuthJWT = Depends()):
     # Do prediction.
+    authorize.jwt_required()
+    user = await user_service.get_by_email(authorize.get_jwt_subject())
+    await log_service.create(user.id, "car", "/saved/photo.jpg")
     return {"results": [{"name": "car", "ranking": "0.9"}, {"name": "truck", "ranking": "0.7"}]}
