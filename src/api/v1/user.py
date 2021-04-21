67c4c1452e2b335a36f1b6ec0593d24e1fb5fd81
Furkan <frkn__asl@windowslive.com>
2021-04-21 04:04:39 +0300

    user endpoints me and history

---
+++ src/api/v1/user.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi_jwt_auth import AuthJWT
+from src.services import user_service, log_service
+from src.models import User, Log
+
+router = APIRouter()
+
+from src.schemas.log import LogSchema
+from pydantic import parse_obj_as
+
+
+@router.get('/me')
+async def user_details():
+    raise NotImplementedError()
+
+
+@router.get('/me/history')
+async def user_history(authorize: AuthJWT = Depends(), skip: int = 0, limit: int = 10):
+    user: User = await user_service.get_by_email(authorize.get_jwt_subject())
+    if not user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Bad Request.")
+    logs = await log_service.get_logs_by_user(user.id, skip=skip, limit=limit)
+    return logs
