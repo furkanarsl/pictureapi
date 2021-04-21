d7aefc10f68cb14986aea2aed0c8eb502132a4e9
Furkan <frkn__asl@windowslive.com>
2021-04-21 04:03:23 +0300

    update upload endpoint with the actual prediction.

--- src/api/v1/predictions.py
+++ src/api/v1/predictions.py
@@ -7,30 +7,34 @@ import pathlib
 from src.services.result_log import log_service
 from src.services.user import user_service
 from src.core.config import settings
+from src.core.prediction import predict, load_img
+from datetime import datetime
+from hashlib import md5
 
 ALLOWED_EXTENSIONS = ["jpg", "jpeg", "png"]
 router = APIRouter()
 
 
-@router.post("/upload")
-async def file_upload(file: UploadFile = File(...)):
+@router.post("/")
+async def predict_img(file: UploadFile = File(...), authorize: AuthJWT = Depends()):
+    # Do prediction.
+    authorize.jwt_required()
+    user = await user_service.get_by_email(authorize.get_jwt_subject())
     try:
         file_name, file_extension = file.filename.split(".")
     except ValueError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not a valid file.")
     if file_extension not in ALLOWED_EXTENSIONS:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="File type not allowed.")
-    save_path = pathlib.Path(settings.SAVE_PATH) / (file_name + "." + file_extension)
+    img_name = md5(str(file_name + str(datetime.now())).encode('utf-8')).hexdigest()
+    save_path = pathlib.Path(settings.SAVE_PATH) / (str(img_name) + "." + file_extension)
+
     async with aiofiles.open(save_path, 'wb') as out_file:
-        while content := await file.read(16777216):  # 16MB
+        while content := await file.read(8388608):  # 16MB
             await out_file.write(content)
-    return {"Result": "OK"}
 
+    img = load_img(f'{save_path}')
+    _, result = predict(img)
 
-@router.post("/", response_model=PredictionResult)
-async def predict(body: PredictionParameters, authorize: AuthJWT = Depends()):
-    # Do prediction.
-    authorize.jwt_required()
-    user = await user_service.get_by_email(authorize.get_jwt_subject())
-    await log_service.create(user.id, "car", "/saved/photo.jpg")
-    return {"results": [{"name": "car", "ranking": "0.9"}, {"name": "truck", "ranking": "0.7"}]}
+    await log_service.create(user.id, result, save_path)
+    return {"result": result}
