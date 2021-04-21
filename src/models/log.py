e6e0b473b7a7935b9460676b3d456d67dcd29c67
Furkan <frkn__asl@windowslive.com>
2021-04-21 04:05:50 +0300

    update log model

--- src/models/log.py
+++ src/models/log.py
@@ -6,6 +6,14 @@ from src.models import User
 
 class Log(Model):
     id = fields.IntField(pk=True, null=False)
+    query_date = fields.DatetimeField(auto_now=True)
     user: fields.ForeignKeyRelation[User] = fields.ForeignKeyField("models.User", related_name='user')
     picture_path = fields.TextField()
     result = fields.TextField()
+
+    def img_path(self) -> str:
+        return self.picture_path.replace("\\", "/")
+
+    class PydanticMeta:
+        exclude = ['id', "picture_path"]
+        computed = ["img_path"]
