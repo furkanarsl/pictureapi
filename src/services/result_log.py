e0eaed1804c6b024722501b626cf7f5d69f1a1e6
Furkan <frkn__asl@windowslive.com>
2021-04-21 04:08:12 +0300

    update log service

--- src/services/result_log.py
+++ src/services/result_log.py
@@ -1,9 +1,20 @@
 from src.models.log import Log
+from src.schemas.log import LogSchemaList
 
 
 class LogService:
     async def create(self, user_id, result, picture_path):
         log = await Log.create(user_id=user_id, result=result, picture_path=picture_path)
         return log
+
+    async def get_logs_by_user(self, user_id: int, skip: int, limit: int):
+        return await LogSchemaList.from_queryset(
+            Log.filter(user_id=user_id).order_by('-query_date').limit(limit=limit).offset(offset=skip))
+        # return await Log.filter(user_id=user_id).order_by('-query_date').all()
+
+    async def get_log_by_img(self, img_name: str):
+        img_path = f"saved\\{img_name}"
+        return await Log.filter(picture_path=img_path).first()
+
 
 log_service = LogService()
