96916e5e5f9c3cfc0acd8c7b40fedcd6c8463951
furkanarsl <frkn__asl@windowslive.com>
2021-04-23 05:48:20 +0300

    format imports

--- src/api/v1/api.py
+++ src/api/v1/api.py
@@ -5,6 +5,8 @@ from src.api.v1 import auth, predictions, user, saved_images
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
-api_router.include_router(predictions.router, prefix="/prediction", tags=["Predictions"])
+api_router.include_router(
+    predictions.router, prefix="/prediction", tags=["Predictions"])
 api_router.include_router(user.router, prefix="/user", tags=["User"])
-api_router.include_router(saved_images.router, prefix="/saved", tags=["Images"])
+api_router.include_router(
+    saved_images.router, prefix="/saved", tags=["Images"])
