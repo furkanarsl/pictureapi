604f19999b09205ea903db969714f981859ad6c3
furkanarsl <frkn__asl@windowslive.com>
2021-04-23 05:50:21 +0300

    get_logs_by_user actually returns log list instead of pydantic model

--- src/services/result_log.py
+++ src/services/result_log.py
@@ -1,3 +1,4 @@
+from typing import List
 from src.models.log import Log
 from src.schemas.log import LogSchemaList
 
@@ -7,10 +8,8 @@ class LogService:
         log = await Log.create(user_id=user_id, result=result, picture_path=picture_path)
         return log
 
-    async def get_logs_by_user(self, user_id: int, skip: int, limit: int) -> LogSchemaList:
-        return await LogSchemaList.from_queryset(
-            Log.filter(user_id=user_id).order_by('-query_date').limit(limit=limit).offset(offset=skip))
-        # return await Log.filter(user_id=user_id).order_by('-query_date').all()
+    async def get_logs_by_user(self, user_id: int, skip: int, limit: int) -> List[Log]:
+        return await Log.filter(user_id=user_id).order_by('-query_date').limit(limit=limit).offset(offset=skip)
 
     async def get_log_by_img(self, img_name: str):
         return await Log.filter(picture_path=img_name).first()
