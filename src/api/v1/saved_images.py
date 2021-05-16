15f8b2f274d72522f9c5c238c416f726f16ec424
furkanarsl <frkn__asl@windowslive.com>
2021-05-16 16:42:34 +0300

    check if image exists before returning

--- src/api/v1/saved_images.py
+++ src/api/v1/saved_images.py
@@ -10,13 +10,18 @@ from src.models import User
 router = APIRouter()
 
 
-@router.get('/{img_name}', response_class=FileResponse)
+@router.get("/{img_name}", response_class=FileResponse)
 async def get_img(img_name, user: User = Depends(get_current_user)):
     img = await log_service.get_log_by_img(img_name)
 
+    if not img:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="File not found."
+        )
     if img.user_id != user.id:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
+        )
 
     path = Path(settings.SAVE_PATH) / img_name
-    return FileResponse(f'{path}')
+    return FileResponse(f"{path}")
