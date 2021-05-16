b1729ce1014cbb8ec9314ee2b4a0dac7e1cc43c7
furkanarsl <frkn__asl@windowslive.com>
2021-05-16 16:42:44 +0300

    redis and smtp config

--- src/core/config.py
+++ src/core/config.py
@@ -7,7 +7,15 @@ class Settings(BaseSettings):
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 180  # DEFAULT 3 HOURS
     JWT_REFRESH_TOKEN_EXPIRE_MINUTES: int = 43200  # DEFAULT 30 DAYS
     SQLALCHEMY_DATABASE_URI: PostgresDsn  # TODO move to .env
-    SAVE_PATH: DirectoryPath = 'saved'
+    SAVE_PATH: DirectoryPath = "saved"
+    SMTP_USER: str
+    SMTP_PASSWORD: str
+    SMTP_HOST: str
+    SMTP_PORT: int
+    SMTP_TLS = True
+    REDIS_URL: str
+    REDIS_PORT: int
+    API_URL: str = "http://192.168.193.31"
 
 
 settings = Settings()
