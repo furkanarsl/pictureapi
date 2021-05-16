2c218c99456500b54e6b14874fb62bdce49287d7
furkanarsl <frkn__asl@windowslive.com>
2021-05-16 16:42:07 +0300

    update deps for verified user

--- src/api/deps.py
+++ src/api/deps.py
@@ -3,19 +3,65 @@ import jwt
 
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends, HTTPException, status
-from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_422_UNPROCESSABLE_ENTITY
 from src.models import User
 from src.core.config import settings
 from src.core import security
 from src.services import user_service
 from src.schemas.token import Token, TokenPayload
 
-reusable_oauth2 = OAuth2PasswordBearer(
-    tokenUrl=f"{settings.API_V1_STR}/auth/login/"
-)
+reusable_oauth2 = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_STR}/auth/login/")
 
 
-async def get_current_user(token: str = Depends(reusable_oauth2)) -> User:
+async def access_token_required(token: str = Depends(reusable_oauth2)) -> Token:
+    payload = jwt.decode(
+        token, settings.JWT_SECRET_KEY, algorithms=[security.ALGORITHM]
+    )
+    token_data = TokenPayload(**payload)
+    if token_data.is_active == "False":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Only verifed users are allowed.",
+        )
+
+    if token_data.type != "access":
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Only access tokens are allowed.",
+        )
+    return token
+
+
+async def access_token_required_non_verified(
+    token: str = Depends(reusable_oauth2),
+) -> Token:
+    payload = jwt.decode(
+        token, settings.JWT_SECRET_KEY, algorithms=[security.ALGORITHM]
+    )
+    token_data = TokenPayload(**payload)
+
+    if token_data.type != "access":
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Only access tokens are allowed.",
+        )
+    return token
+
+
+async def refresh_token_required(token: str = Depends(reusable_oauth2)):
+    payload = jwt.decode(
+        token, settings.JWT_SECRET_KEY, algorithms=[security.ALGORITHM]
+    )
+    token_data = TokenPayload(**payload)
+    if token_data.type != "refresh":
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Only refresh tokens are allowed.",
+        )
+    return token
+
+
+async def get_current_user(token: str = Depends(access_token_required)) -> User:
     payload = jwt.decode(
         token, settings.JWT_SECRET_KEY, algorithms=[security.ALGORITHM]
     )
@@ -26,28 +72,3 @@ async def get_current_user(token: str = Depends(reusable_oauth2)) -> User:
         raise HTTPException(status_code=404, detail="User not found")
 
     return user
-
-
-async def access_token_required(token: str = Depends(reusable_oauth2)) -> Token:
-    payload = jwt.decode(
-        token, settings.JWT_SECRET_KEY, algorithms=[security.ALGORITHM]
-    )
-    token_data = TokenPayload(**payload)
-
-    if token_data.type == "access":
-        return token
-    else:
-        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                            detail="Only access tokens are allowed.")
-
-
-async def refresh_token_required(token: str = Depends(reusable_oauth2)):
-    payload = jwt.decode(
-        token, settings.JWT_SECRET_KEY, algorithms=[security.ALGORITHM]
-    )
-    token_data = TokenPayload(**payload)
-    if token_data.type == "refresh":
-        return token
-    else:
-        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                            detail="Only refresh tokens are allowed.")
