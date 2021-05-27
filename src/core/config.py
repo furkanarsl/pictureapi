0df200e1972730304c8b738f56bfbf676ba70d95
furkanarsl <frkn__asl@windowslive.com>
2021-05-27 22:58:24 +0300

    remove TODO

--- src/core/config.py
+++ src/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 180  # DEFAULT 3 HOURS
     JWT_REFRESH_TOKEN_EXPIRE_MINUTES: int = 43200  # DEFAULT 30 DAYS
-    SQLALCHEMY_DATABASE_URI: PostgresDsn  # TODO move to .env
+    SQLALCHEMY_DATABASE_URI: PostgresDsn
     SAVE_PATH: DirectoryPath = "saved"
     SMTP_USER: str
     SMTP_PASSWORD: str
